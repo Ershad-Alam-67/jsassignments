d7c3c78902b42e8e0f01872620642bb6988986b0
Ershad-Alam-67 <ealam6732@gmail.com>
2023-05-18 19:01:04 +0530

    new file added for todolist

---
+++ index.js
@@ -0,0 +1,5 @@
+const header = document.getElementById("main-header");
+header.style.borderBottom = "solid 3px #000";
+const tit = document.getElementById("id");
+tit.style.fontWeight = "bold";
+tit.style.color = "green";
