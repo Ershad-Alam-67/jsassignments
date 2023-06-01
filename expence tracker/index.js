65e592a7929a67db4572d3667e267c4560cb48e1
Ershad-Alam-67 <ealam6732@gmail.com>
2023-06-02 00:18:55 +0530

    Making an Expense tracker app with local storage integration

--- expence tracker/index.js
+++ expence tracker/index.js
@@ -1,93 +1,123 @@
 document.addEventListener("DOMContentLoaded", function () {
   const add = document.getElementById("add")
-  add.addEventListener("click", function () {
-    const am = document.getElementById("am").value
-    const de = document.getElementById("de").value
-    const ca = document.getElementById("ca").value
+  add.addEventListener("click", addExpense)
 
-    // Create an object to store the expense details
-    const expense = {
-      amount: am,
-      description: de,
-      category: ca,
-    }
-
-    // Retrieve existing expenses from localStorage or initialize an empty array
-    const existingExpenses = JSON.parse(localStorage.getItem("expenses")) || []
-
-    // Add the new expense to the array
-    existingExpenses.push(expense)
-
-    // Store the updated expenses array in localStorage
-    localStorage.setItem("expenses", JSON.stringify(existingExpenses))
-
-    // Clear the input fields after storing the data
-    document.getElementById("am").value = ""
-    document.getElementById("de").value = ""
-    document.getElementById("ca").value = "none"
-
-    // Create and append the list item for the new expense
-    const li = document.createElement("li")
-    li.setAttribute("class", "row list-group-item d-flex")
-
-    const p = document.createElement("p")
-    p.setAttribute("class", "col-4")
-    p.textContent = am + "-" + ca + "-" + de
-
-    const div = document.createElement("div")
-    div.setAttribute("class", "offset-3 col-5 d-flex justify-content-end")
-
-    const edit = document.createElement("input")
-    edit.setAttribute("type", "button")
-    edit.setAttribute("value", "Edit")
-    edit.setAttribute("class", "btn btn-primary")
-
-    const dlt = document.createElement("input")
-    dlt.setAttribute("type", "button")
-    dlt.setAttribute("value", "Delete")
-    dlt.setAttribute("class", "btn btn-danger")
-
-    div.appendChild(edit)
-    div.appendChild(dlt)
-    li.appendChild(p)
-    li.appendChild(div)
-
-    const ul = document.getElementById("ui")
-    ul.appendChild(li)
-  })
   const storedExpenses = localStorage.getItem("expenses")
   if (storedExpenses) {
     const expenses = JSON.parse(storedExpenses)
     expenses.forEach(function (expense) {
       const { amount, description, category } = expense
-
-      const li = document.createElement("li")
-      li.setAttribute("class", "row list-group-item d-flex")
-
-      const p = document.createElement("p")
-      p.setAttribute("class", "col-4")
-      p.textContent = amount + "-" + category + "-" + description
-
-      const div = document.createElement("div")
-      div.setAttribute("class", "offset-3 col-5 d-flex justify-content-end")
-
-      const edit = document.createElement("input")
-      edit.setAttribute("type", "button")
-      edit.setAttribute("value", "Edit")
-      edit.setAttribute("class", "btn btn-primary")
-
-      const dlt = document.createElement("input")
-      dlt.setAttribute("type", "button")
-      dlt.setAttribute("value", "Delete")
-      dlt.setAttribute("class", "btn btn-danger")
-
-      div.appendChild(edit)
-      div.appendChild(dlt)
-      li.appendChild(p)
-      li.appendChild(div)
-
-      const ul = document.getElementById("ui")
-      ul.appendChild(li)
+      createExpenseItem(amount, description, category)
     })
   }
 })
+
+function createExpenseItem(amount, description, category) {
+  const li = document.createElement("li")
+  li.setAttribute("class", "row list-group-item d-flex")
+
+  const p = document.createElement("p")
+  p.setAttribute("class", "col-7")
+  p.textContent = `${amount} - ${category} - ${description}`
+
+  const div = document.createElement("div")
+  div.setAttribute("class", "col-5 d-flex justify-content-end")
+
+  const edit = document.createElement("input")
+  edit.setAttribute("type", "button")
+  edit.setAttribute("value", "Edit")
+  edit.setAttribute("class", "btn btn-primary")
+  edit.addEventListener("click", editExpense)
+
+  const dlt = document.createElement("input")
+  dlt.setAttribute("type", "button")
+  dlt.setAttribute("value", "Delete")
+  dlt.setAttribute("class", "btn btn-danger")
+  dlt.addEventListener("click", deleteExpense)
+
+  div.appendChild(edit)
+  div.appendChild(dlt)
+  li.appendChild(p)
+  li.appendChild(div)
+
+  const ul = document.getElementById("ui")
+  ul.appendChild(li)
+}
+
+function addExpense() {
+  const am = document.getElementById("am").value
+  const de = document.getElementById("de").value
+  const ca = document.getElementById("ca").value
+
+  const expense = {
+    amount: am,
+    description: de,
+    category: ca,
+  }
+
+  const existingExpenses = JSON.parse(localStorage.getItem("expenses")) || []
+  existingExpenses.push(expense)
+  localStorage.setItem("expenses", JSON.stringify(existingExpenses))
+
+  document.getElementById("am").value = ""
+  document.getElementById("de").value = ""
+  document.getElementById("ca").value = "none"
+
+  createExpenseItem(am, de, ca)
+}
+
+function editExpense(e) {
+  const listItem = e.target.parentNode.parentNode
+  const p = listItem.querySelector("p")
+  const editValue = p.textContent.split("-").map((value) => value.trim())
+
+  const am = document.getElementById("am")
+  const de = document.getElementById("de")
+  const ca = document.getElementById("ca")
+
+  am.value = editValue[0]
+  ca.value = editValue[1]
+  de.value = editValue[2]
+
+  const add = document.getElementById("add")
+  add.textContent = "Update"
+  add.removeEventListener("click", addExpense)
+  add.addEventListener("click", updateExpense.bind(null, listItem))
+}
+
+function updateExpense(listItem) {
+  const am = document.getElementById("am").value
+  const de = document.getElementById("de").value
+  const ca = document.getElementById("ca").value
+
+  const p = listItem.querySelector("p")
+  p.textContent = `${am} - ${ca} - ${de}`
+
+  const ul = document.getElementById("ui")
+  const index = Array.from(ul.children).indexOf(listItem)
+  const storedExpenses = JSON.parse(localStorage.getItem("expenses")) || []
+  storedExpenses[index].amount = am
+  storedExpenses[index].description = de
+  storedExpenses[index].category = ca
+  localStorage.setItem("expenses", JSON.stringify(storedExpenses))
+
+  document.getElementById("am").value = ""
+  document.getElementById("de").value = ""
+  document.getElementById("ca").value = "none"
+
+  const add = document.getElementById("add")
+  add.textContent = "Add Expense"
+  add.removeEventListener("click", updateExpense)
+  add.addEventListener("click", addExpense)
+}
+
+function deleteExpense(e) {
+  const listItem = e.target.parentNode.parentNode
+  const ul = listItem.parentNode
+  ul.removeChild(listItem)
+
+  const index = Array.from(ul.children).indexOf(listItem)
+  const storedExpenses = JSON.parse(localStorage.getItem("expenses")) || []
+  storedExpenses.splice(index, 1)
+  localStorage.setItem("expenses", JSON.stringify(storedExpenses))
+}
