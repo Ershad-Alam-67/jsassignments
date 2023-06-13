e4185f58600d62666722bb3d1bdb1b0c2501df21
Ershad-Alam-67 <ealam6732@gmail.com>
2023-06-13 21:30:07 +0530

    Update the user Details

--- crud/index.js
+++ crud/index.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", update)
+let tobeupdate = ""
+let tobeupdateli = ""
 
 function add(e) {
   const nm = document.getElementById("name").value
@@ -12,11 +14,14 @@ function add(e) {
 
   axios
     .post(
-      "https://crudcrud.com/api/7ac295fbe0b44140a742a02bfef18830/ershad",
+      "https://crudcrud.com/api/bb3ead1ceafd4bb097cac8fdc23c8e34/ershad",
       obj
     )
     .then(() => {
       console.log("posted")
+      document.getElementById("name").value = ""
+      document.getElementById("mail").value = ""
+      document.getElementById("phn").value = ""
       update()
     })
     .catch((err) => console.log(err))
@@ -29,13 +34,20 @@ function createDeleteButton(liId) {
   dlt.addEventListener("click", () => deleteItem(liId))
   return dlt
 }
+function createEditButton(liId) {
+  const dlt = document.createElement("button")
+  dlt.setAttribute("class", "btn btn-primary m-1")
+  dlt.textContent = "Edit"
+  dlt.addEventListener("click", () => editItem(liId))
+  return dlt
+}
 
 function update() {
   const ui = document.getElementById("ui")
   ui.innerHTML = ""
 
   axios
-    .get("https://crudcrud.com/api/7ac295fbe0b44140a742a02bfef18830/ershad")
+    .get("https://crudcrud.com/api/bb3ead1ceafd4bb097cac8fdc23c8e34/ershad")
     .then((res) => {
       const data = res.data
       console.log(data)
@@ -46,8 +58,10 @@ function update() {
         li.setAttribute("id", `li-${index}`)
 
         const deleteButton = createDeleteButton(`li-${index}`)
+        const editButton = createEditButton(`li-${index}`)
 
         li.textContent = `${item.name} - ${item.email} - ${item.phone}`
+        li.appendChild(editButton)
         li.appendChild(deleteButton)
 
         ui.appendChild(li)
@@ -59,7 +73,7 @@ function update() {
 function deleteItem(liId) {
   const li = document.getElementById(liId)
   axios
-    .get("https://crudcrud.com/api/7ac295fbe0b44140a742a02bfef18830/ershad")
+    .get("https://crudcrud.com/api/bb3ead1ceafd4bb097cac8fdc23c8e34/ershad")
     .then((res) => {
       const data = res.data
       const index = liId.split("-")[1]
@@ -72,13 +86,73 @@ function deleteItem(liId) {
     .then((id) => {
       axios
         .delete(
-          `https://crudcrud.com/api/7ac295fbe0b44140a742a02bfef18830/ershad/${id}`
+          `https://crudcrud.com/api/bb3ead1ceafd4bb097cac8fdc23c8e34/ershad/${id}`
         )
 
         .then(() => {
           li.remove()
+
           update()
         })
     })
     .catch((err) => console.log(err))
+}
+function editItem(liId) {
+  axios
+    .get("https://crudcrud.com/api/bb3ead1ceafd4bb097cac8fdc23c8e34/ershad")
+    .then((res) => {
+      const data = res.data
+      console.log(data)
+      const index = liId.split("-")[1]
+      const id = data[index]._id
+      tobeupdate = id
+      tobeupdateli = document.getElementById(liId)
+      console.log(index)
+
+      return new Promise((res, rej) => {
+        res({ id, index, data })
+      })
+    })
+
+    .then(({ id, index, data }) => {
+      const sub = document.getElementById("sub")
+      sub.setAttribute("onclick", "updt()")
+
+      sub.textContent = "Update"
+
+      document.getElementById("name").value = data[index].name
+      document.getElementById("mail").value = data[index].email
+      document.getElementById("phn").value = data[index].phone
+    })
+    .catch((err) => console.log(err))
+}
+
+function updt() {
+  const nm = document.getElementById("name").value
+  const em = document.getElementById("mail").value
+  const ph = document.getElementById("phn").value
+  let obj = {
+    name: nm,
+    email: em,
+    phone: ph,
+  }
+  axios
+    .put(
+      `https://crudcrud.com/api/bb3ead1ceafd4bb097cac8fdc23c8e34/ershad/${tobeupdate}`,
+      obj
+    )
+
+    .then(() => {
+      tobeupdateli.remove()
+      const sub = document.getElementById("sub")
+      sub.textContent = "Submit"
+      sub.setAttribute("onclick", "add()")
+      document.getElementById("name").value = ""
+      document.getElementById("mail").value = ""
+      document.getElementById("phn").value = ""
+      update()
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
