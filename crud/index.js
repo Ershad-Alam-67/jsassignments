bda8d11ea08a86b4ed2604e4ebc540ad353f202f
Ershad-Alam-67 <ealam6732@gmail.com>
2023-06-13 18:43:41 +0530

    Deleting the Appointments

--- crud/index.js
+++ crud/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", update)
+
 function add(e) {
   const nm = document.getElementById("name").value
   const em = document.getElementById("mail").value
@@ -8,35 +9,76 @@ function add(e) {
     email: em,
     phone: ph,
   }
+
   axios
     .post(
-      "https://crudcrud.com/api/e26a443d50be4413b15eb8fdf9a44b0f/ershad",
+      "https://crudcrud.com/api/7ac295fbe0b44140a742a02bfef18830/ershad",
       obj
     )
-    .then(() => console.log("posted"))
+    .then(() => {
+      console.log("posted")
+      update()
+    })
     .catch((err) => console.log(err))
-  update()
 }
-function makeli() {
-  const li = document.createElement("li")
-  li.setAttribute("class", "list-group-item")
-  return li
+
+function createDeleteButton(liId) {
+  const dlt = document.createElement("button")
+  dlt.setAttribute("class", "btn btn-danger")
+  dlt.textContent = "Delete"
+  dlt.addEventListener("click", () => deleteItem(liId))
+  return dlt
 }
+
 function update() {
   const ui = document.getElementById("ui")
   ui.innerHTML = ""
-  let data = ""
+
   axios
-    .get("https://crudcrud.com/api/e26a443d50be4413b15eb8fdf9a44b0f/ershad")
+    .get("https://crudcrud.com/api/7ac295fbe0b44140a742a02bfef18830/ershad")
     .then((res) => {
-      data = res.data
+      const data = res.data
       console.log(data)
-      data.forEach((item) => {
-        const li = makeli()
-        li.textContent = `${item.name}-${item.email}-${item.phone}`
+
+      data.forEach((item, index) => {
+        const li = document.createElement("li")
+        li.setAttribute("class", "list-group-item")
+        li.setAttribute("id", `li-${index}`)
+
+        const deleteButton = createDeleteButton(`li-${index}`)
+
+        li.textContent = `${item.name} - ${item.email} - ${item.phone}`
+        li.appendChild(deleteButton)
+
         ui.appendChild(li)
       })
     })
+    .catch((err) => console.log(err))
+}
 
+function deleteItem(liId) {
+  const li = document.getElementById(liId)
+  axios
+    .get("https://crudcrud.com/api/7ac295fbe0b44140a742a02bfef18830/ershad")
+    .then((res) => {
+      const data = res.data
+      const index = liId.split("-")[1]
+      const id = data[index]._id
+      return new Promise((res, rej) => {
+        res(id)
+      })
+    })
+
+    .then((id) => {
+      axios
+        .delete(
+          `https://crudcrud.com/api/7ac295fbe0b44140a742a02bfef18830/ershad/${id}`
+        )
+
+        .then(() => {
+          li.remove()
+          update()
+        })
+    })
     .catch((err) => console.log(err))
 }
